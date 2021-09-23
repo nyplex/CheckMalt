fed0ded407c27f2940f6706efd0bd044ffa48233
Alex <nypels.alexandre@outlook.com>
2021-09-23 14:36:00 +0100

    creating Category App and creating the category model

---
+++ category/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    description = models.CharField(max_length=500, blank=True)
+    image = models.ImageField(upload_to='images/categories', blank=True)
+
+    def __str__(self):
+        return self.name#
+    
+    class Meta:
+        verbose_name_plural = "Categories"
