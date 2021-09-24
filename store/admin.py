f64b6378ed968083c0586951b00ef3dcc212f4cb
Alex <nypels.alexandre@outlook.com>
2021-09-24 14:01:42 +0100

    database bugs

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.db import models
-from .models import Product
+#from .models import Product
 
 # Register your models here.
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock', 'is_available')
-    prepopulated_fields = {'slug': ('name',)}
+#class ProductAdmin(admin.ModelAdmin):
+ #   list_display = ('name', 'price', 'stock', 'is_available')
+  #  prepopulated_fields = {'slug': ('name',)}
 
 
-admin.site.register(Product, ProductAdmin)
+#admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -1,21 +1,8 @@
 from django.db.models.fields.related import ForeignKey
 from category.models import Category
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    slug = models.SlugField(max_length=255, unique=True)
-    description = models.TextField(max_length=1000, blank=True)
-    price = models.FloatField()
-    images = models.ImageField(upload_to='images/products')
-    stock = models.IntegerField()
-    is_available = models.BooleanField(default=True)
-    category = models.ManyToManyField(Category)
-    ceated_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
