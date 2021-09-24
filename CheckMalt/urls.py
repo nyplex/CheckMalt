9fafba03406676f3a1ffaa8551109f3562205fa4
Alex <nypels.alexandre@outlook.com>
2021-09-24 01:16:35 +0100

    creating store.html file and displaying products in Store page

--- CheckMalt/urls.py
+++ CheckMalt/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from CheckMalt.settings import MEDIA_ROOT
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Home, name='home')
+    path('', views.Home, name='home'),
+    path('store/', include('store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
+from store.models import Product
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
+    return render(request, 'store/store.html', {
+        'products': products,
+        'product_count': product_count
+    })
