6937ec99c25956b6ab9ec0beb44518666b68514e
Alex <nypels.alexandre@outlook.com>
2021-09-28 01:07:49 +0100

    adding registration&login feature + messages alert

--- CheckMalt/urls.py
+++ CheckMalt/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home, name='home'),
     path('store/', include('store.urls')),
-    path('cart/', include('cart.urls'))
+    path('cart/', include('cart.urls')),
+    path('account/', include('accounts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
